4aa300012e099c29f19e0d2a6b1827e71f9caa70
isabellademetz <isabella21136@gmail.com>
2018-07-11 16:00:27 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,8 +1,9 @@
  time = 12
+ curfew = 11
 if time > 11
   puts "You're in trouble! Better get home quick!"
 elsif time = 11
   puts "You're in trouble! Better get home quick!"
 else
-  puts "Keep having fun!"
+  puts "You have 
 end
