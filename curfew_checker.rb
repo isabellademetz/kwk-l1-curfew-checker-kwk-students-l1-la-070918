c89dcca44eb2229dc6d23b7b9994998c4e04e445
isabellademetz <isabella21136@gmail.com>
2018-07-11 15:55:51 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,4 +1,8 @@
  time = 12
-if time >= 11
-  puts "time is greater than or equal to 11."
+if time > 11
+  puts "time is greater than  11."
+elsif time = 11
+  puts "you're in trouble if it's 11"
+else
+  
 end
