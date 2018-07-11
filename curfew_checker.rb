3757904fe24233aba15664633aad627966f1b35d
isabellademetz <isabella21136@gmail.com>
2018-07-11 16:07:13 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,10 +1,10 @@
  time = 10
- curfew = #{your time}
+ curfew = #{your curfew}
  curfew = 11
 if time > 11
   puts "You're in trouble! Better get home quick!"
 elsif time == 11
   puts "Time to apparate"
 else
-  puts "You have #{curfew - time} hour(s) left"
+  puts "Keep having fun. Curfew is #{your curfew} and time is #{time}. There is #{curfew - time} hour(s) left."
 end
